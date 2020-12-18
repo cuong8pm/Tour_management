0438b25c3a041c9bad3633ebcfa4f275ba1ccbde
giang-dam-minh <minhgiang851@gmail.com>
2020-12-18 15:58:42 +0700

    first test

---
+++ BTL_LTWEB/BTL_LTWEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BTL_LTWEB.MvcApplication" Language="C#" %>
---
+++ BTL_LTWEB/BTL_LTWEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BTL_LTWEB.Startup))]
+namespace BTL_LTWEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
